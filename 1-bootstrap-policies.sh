a23f364fad0fa427ee3efcdd129de4f532ff6a58
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-05-11 13:53:26 -0700

    Update secret name for eso

--- 1-bootstrap-policies.sh
+++ 1-bootstrap-policies.sh
@@ -1,3 +1,3 @@
 kustomize build bootstrap/aggregate/overlays/policies-and-secrets | oc apply -f -
 oc apply -f sealed-secrets-key.yaml
-oc apply -f eso-token-secret.yaml
+oc apply -f eso-token-common-secret.yaml
