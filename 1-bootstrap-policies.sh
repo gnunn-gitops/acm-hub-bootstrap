14bc344b5dc2943d91e673a3888ef4b44ab0315c
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-05-11 17:11:00 -0700

    Update policy to use templated secrets

--- 1-bootstrap-policies.sh
+++ 1-bootstrap-policies.sh
@@ -1,3 +1,4 @@
 kustomize build bootstrap/aggregate/overlays/policies-and-secrets | oc apply -f -
 oc apply -f sealed-secrets-key.yaml
-oc apply -f eso-token-common-secret.yaml
+oc apply -f eso-token-cluster-hub-secret.yaml
+oc apply -f eso-token-cluster-home-secret.yaml
