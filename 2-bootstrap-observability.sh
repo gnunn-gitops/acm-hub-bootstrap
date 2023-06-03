29e6a9df3f037ead6ac1fce063bb277e9dffd2a8
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-06-03 09:34:58 -0700

    No longer needed

--- 2-bootstrap-observability.sh
+++
@@ -1,3 +0,0 @@
-# Optional to deploy observability component of ACM
-# This is a minimal deployment that uses Minio running on my homelab QNAP NAS
-kustomize build components/apps/acm/overlays/observability | oc apply -f -
