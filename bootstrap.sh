55864fa8b653ec3fd3af31592856e1a66365ee89
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-06-03 19:00:50 -0700

    Tweak bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LANG=C
-SLEEP_SECONDS=10
+SLEEP_SECONDS=30
 
 echo ""
 echo "Installing RHACM Operator."
@@ -14,7 +14,7 @@ sleep $SLEEP_SECONDS
 echo "Waiting for operator to start"
 until oc get deployment multiclusterhub-operator -n open-cluster-management
 do
-  sleep 5;
+  sleep 10;
 done
 
 echo "Installing the MultiClusterHub"
@@ -25,8 +25,9 @@ echo "Waiting for hub to be installed"
 
 until [[ $(oc get multiclusterhub multiclusterhub -n open-cluster-management -o jsonpath='{.status.phase}') == 'Running' ]]
 do
-  echo "Waiting for hub..."
-  sleep 5
+  echo "Waiting for hub, current status:"
+  oc get multiclusterhub multiclusterhub -n open-cluster-management
+  sleep 10
 done
 
 echo "Installing policies and initial secrets"
@@ -36,6 +37,9 @@ kustomize build bootstrap/policies/overlays/default --enable-alpha-plugins | oc 
 
 echo "Labeling cluster with 'gitops: local.home'"
 oc label managedcluster local-cluster gitops=local.hub --overwrite=true
+
+echo "Check policy compliance with the following command:"
+echo "  oc get policy -A"
 
 echo "Once GitOps configuration is complete you may need to run the following to fix Unknown status\n"
 echo "  oc delete secret local-cluster-import -n local-cluster\n"
